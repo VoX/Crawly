b08faac63df63546ebc0b94ea96eebeaac05a749
Ian Hills <ihills@protonmail.com>
2016-04-23 15:49:12 -0400

    add basic robots.txt support

--- Crawly.UnitTests/CrawlerEngineTests.cs
+++ Crawly.UnitTests/CrawlerEngineTests.cs
@@ -1,4 +1,5 @@
-﻿ using System.Linq;
+﻿ using System;
+ using System.Linq;
  using NUnit.Framework;
 
 namespace Crawly.UnitTests
@@ -10,18 +11,27 @@ namespace Crawly.UnitTests
         public void GetLinks_ReturnsLinks()
         {
             var engine = new CrawlerEngine();
-            var links = engine.GetLinks("https://www.penny-arcade.com/comic").Result;
-            Assert.True(links.Any(x=>x.Contains("http://paxsite.com/")), "http://paxsite.com/");
-            Assert.True(links.Any(x => x.Contains("http://forums.penny-arcade.com/")), "http://forums.penny-arcade.com/");
-            Assert.True(links.Any(x => x.Contains("https://www.penny-arcade.com/clubpa")), "/clubpa");
-            Assert.False(links.Any(x => x.Contains("https://www.penny-arcade.com/archive/results/search")), "https://www.penny-arcade.com/archive/results/search");
+            var links = engine.GetLinks(new Uri("https://www.penny-arcade.com/comic")).Result;
+            Assert.True(links.Any(x=>x.OriginalString.Contains("http://paxsite.com/")), "http://paxsite.com/");
+            Assert.True(links.Any(x => x.OriginalString.Contains("http://forums.penny-arcade.com/")), "http://forums.penny-arcade.com/");
+            Assert.True(links.Any(x => x.OriginalString.Contains("https://www.penny-arcade.com/clubpa")), "/clubpa");
+            Assert.False(links.Any(x => x.OriginalString.Contains("https://www.penny-arcade.com/archive/results/search")), "https://www.penny-arcade.com/archive/results/search");
         }
 
         [Test]
-        public void Crawl_Debuglol()
+        public void DisallowedLinks()
         {
             var engine = new CrawlerEngine();
-            var x = engine.Crawl("http://www.google.com").Result;
+            var links = engine.RobotRestrictions(new Uri("https://www.penny-arcade.com/comic")).Result;
+            Assert.True(links.Any(x => x == "/feed/"), "/feed/");
+        }
+
+        [Test]
+        public void IsRobotRestricted()
+        {
+            var engine = new CrawlerEngine();
+            Assert.False(engine.IsRobotRestricted(new Uri("https://www.penny-arcade.com/comic")).Result);
+            Assert.True(engine.IsRobotRestricted(new Uri("https://www.penny-arcade.com/feed/test")).Result);
         }
     }
 }
--- Crawly/CrawlerEngine.cs
+++ Crawly/CrawlerEngine.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -13,22 +16,37 @@ namespace Crawly
         private readonly Regex _linkRegex = new Regex("((?:href=\"(?<protocol>/|http://|https://))(?<link>.+?)[\"])",
             RegexOptions.Compiled & RegexOptions.CultureInvariant, TimeSpan.FromMilliseconds(10));
 
-        private readonly int _maxConcurrency;
+        private readonly ConcurrentDictionary<string, string[]> _robotRestricted =
+            new ConcurrentDictionary<string, string[]>();
 
-        public CrawlerEngine(int concurrency = 10)
+        private int? _maxConcurrency;
+
+        private int MaxConcurrency
         {
-            _maxConcurrency = concurrency;
+            get
+            {
+                if (!_maxConcurrency.HasValue)
+                {
+                    int concurrency;
+                    _maxConcurrency = !int.TryParse(ConfigurationManager.AppSettings["MaxConcurrency"],
+                        out concurrency)
+                        ? 10
+                        : concurrency;
+                }
+
+                return _maxConcurrency.Value;
+            }
         }
 
         public async Task<int> Crawl(string startUrl)
         {
-            var urls = new Stack<string>();
-            var tasks = new List<Task<List<string>>>();
-            urls.Push(startUrl);
+            var urls = new Stack<Uri>();
+            var tasks = new List<Task<List<Uri>>>();
+            urls.Push(new Uri(startUrl));
 
             while (urls.Count > 0)
             {
-                while (tasks.Count < _maxConcurrency && urls.Count > 0)
+                while (tasks.Count < MaxConcurrency && urls.Count > 0)
                 {
                     tasks.Add(CrawlAndProcess(urls.Pop()));
                 }
@@ -36,23 +54,55 @@ namespace Crawly
                 tasks.Remove(completed);
                 foreach (var link in completed.Result)
                 {
-                    urls.Push(link);
+                    var restricted = await IsRobotRestricted(link);
+                    if (!restricted)
+                    {
+                        urls.Push(link);
+                    }
                 }
             }
-           
+
             return _crawledUrls.Count;
         }
 
-        public async Task<List<string>> CrawlAndProcess(string url)
+        public async Task<string[]> RobotRestrictions(Uri url)
+        {
+            var client = new HttpClient();
+            var response = await client.GetStringAsync(url.Scheme + Uri.SchemeDelimiter + url.Host + "/robots.txt");
+            return (from line in response.Split('\n')
+                where line.StartsWith("Disallow:")
+                let disallowed = line.Substring("Disallow:".Length).Trim()
+                where !string.IsNullOrWhiteSpace(disallowed)
+                select disallowed).ToArray();
+        }
+
+        public async Task<bool> IsRobotRestricted(Uri url)
+        {
+            if (!_robotRestricted.ContainsKey(url.Host))
+            {
+                try
+                {
+                    var restrictions = await RobotRestrictions(url);
+                    _robotRestricted.AddOrUpdate(url.Host, s => restrictions, (s, strings) => restrictions);
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+            return _robotRestricted[url.Host].Any(prefix => url.PathAndQuery.StartsWith(prefix));
+        }
+
+        public async Task<List<Uri>> CrawlAndProcess(Uri url)
         {
             var newLinks = await GetLinks(url);
-            var returnLinks = new List<string>();
+            var returnLinks = new List<Uri>();
             foreach (var newLink in newLinks)
             {
-                if (!_crawledUrls.Contains(newLink))
+                if (!_crawledUrls.Contains(newLink.OriginalString))
                 {
-                    PrintFoundMessage(newLink);
-                    _crawledUrls.Add(newLink);
+                    PrintFoundMessage(newLink.OriginalString);
+                    _crawledUrls.Add(newLink.OriginalString);
                     returnLinks.Add(newLink);
                 }
             }
@@ -81,12 +131,11 @@ namespace Crawly
         }
 
 
-        public async Task<List<string>> GetLinks(string url)
+        public async Task<List<Uri>> GetLinks(Uri url)
         {
-            PrintCrawlMessage(url);
-            var parsedUrl = new Uri(url);
+            PrintCrawlMessage(url.OriginalString);
             var client = new HttpClient();
-            var returnLinks = new List<string>();
+            var returnLinks = new List<Uri>();
             string response;
 
             try
@@ -95,7 +144,7 @@ namespace Crawly
             }
             catch (Exception ex)
             {
-                PrintErrorMessage(ex, url);
+                PrintErrorMessage(ex, url.OriginalString);
                 return returnLinks;
             }
 
@@ -106,11 +155,11 @@ namespace Crawly
                 var currProtocol = link.Groups["protocol"].Value;
                 if (currProtocol == "/")
                 {
-                    returnLinks.Add(parsedUrl.Scheme + "://" + parsedUrl.Host + currProtocol + currLink);
+                    returnLinks.Add(new Uri(url.Scheme + Uri.SchemeDelimiter + url.Host + currProtocol + currLink));
                 }
                 else
                 {
-                    returnLinks.Add(currProtocol + currLink);
+                    returnLinks.Add(new Uri(currProtocol + currLink));
                 }
             }
             return returnLinks;
