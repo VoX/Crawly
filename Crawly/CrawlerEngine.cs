dca1a51b5ce13161a68030ad3b9b1f30d2016e6c
Ian Hills <ihills@protonmail.com>
2016-04-23 17:09:35 -0400

    optimize httpclient

--- Crawly/CrawlerEngine.cs
+++ Crawly/CrawlerEngine.cs
@@ -3,6 +3,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -23,6 +24,7 @@ namespace Crawly
 
         public CrawlerEngine()
         {
+            ServicePointManager.DefaultConnectionLimit = MaxConcurrency+100;
             _client.Timeout = TimeSpan.FromSeconds(5);
         }
 
